388406f160e85e21a91c8f9512d546b966d04dd1
johansg93 <121643854+johansg93@users.noreply.github.com>
2025-03-05 11:44:18 +0100

    Added function for getting the users choice, making it lower case. Also added variables for human score and computer score.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log("Hello World");
 
+
+/*
+Math.random() genererar ett tal mellan 0 och 0.999....
+Math.random() * 3 skalar upp det till ett tal mellan 0 och 2.999....
+Math.floor() gör om det till ett heltal: 0, 1 eller 2.
++1 gör att vi får siffrorna 1, 2 eller 3. (ist för 0, 1,2)
+switch-satsen returnerar rätt sträng beroende på siffran.
+Så om den slumpmässiga siffran är:
+
+1 → "Rock"
+2 → "Scissors"
+3 → "Paper"
+*/
 function getComputerChoice() {
     let nummer = Math.floor(Math.random() * 3) + 1;
 
@@ -13,4 +26,13 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("Choose: Rock - Paper - Scissors.").toLowerCase();
+
+    return humanChoice;
+}
+let humanScore = 0;
+let computerScore = 0;
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
